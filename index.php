2f69a552d8b6662930814637cd2e49311fa21e6f
dominuxpres <supertomaroma@gmail.com>
2020-05-01 10:26:02 +0000

    Home

---
+++ index.php
@@ -0,0 +1,117 @@
+<?php
+  include 'head.php';
+?>
+  <body class="my-body">
+  <div class="container header-container">
+    <header class="top-header">
+    <nav class="header-nav">
+        <div class="logo">
+            <a href="index.html">RW</a>
+        </div>
+        <div class="navigation">
+        <ul>
+            <li><a href="about.php">About Rochelle</a></li>
+            <li><a href="books.php">Books</a></li>
+            <li><a href="amazon.com">Shop</a></li>
+            <li><a href="blog.com">Blog</a></li>
+            <li><a href="contact.php">Connect</a></li>
+        </ul>
+        </div>
+    </nav>
+      <figure class="container">
+          <img src="img/banner.png" class="img-fluid" alt="banner image">
+      </figure>
+      <div class="subscribe">
+        <b class="sub-title">Subscribe for our newsletter</b><br>
+        Subscribe to my newsletter and be the first to know of my latest release.<br>
+        <input type="text" placeholder="Enter your name"><br>
+        <input type="text" placeholder="Enter your email">
+        <button class="btn btn-primary" type="submit">Subscribe</button>
+      </div>
+    </header>
+  </div>
+  <div class="intro-video text-center">
+      <div class="card card-video">
+        <h3 class="padding-50">Introductory Video</h3>
+        <iframe class="my-video" width="950" height="500" src="https://www.youtube.com/embed/m1M2y3UgNWw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+  </div>
+  <div class="container-fluid featured text-center">
+      <div class="card featured-card">
+        <h3>Featured Titles</h3>
+      <div class="row">
+          <div class="col-12 col-sm-4 col-md-4">
+            <img src="img/featured.jpg" width="300" class="img-fluid" alt="Book on a table">
+            <h6>Book Title</h6>
+            <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit accusantium sunt pariatur praesentium possimus voluptate odit ea amet consectetur, totam voluptatem veniam beatae quo deleniti culpa. Soluta sunt quibusdam repellendus.</P>
+          </div>
+          <div class="col-12 col-sm-4">
+            <img src="img/featured.jpg" width="300" class="img-fluid" alt="Book on a table">
+            <h6>Book Title</h6>
+            <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit accusantium sunt pariatur praesentium possimus voluptate odit ea amet consectetur, totam voluptatem veniam beatae quo deleniti culpa. Soluta sunt quibusdam repellendus.</P>
+            </div>
+          <div class="col-12 col-sm-4 col-md-4">
+            <img src="img/featured.jpg" width="300" class="img-fluid" alt="Book on a table">
+            <h6>Book Title</h6>
+            <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit accusantium sunt pariatur praesentium possimus voluptate odit ea amet consectetur, totam voluptatem veniam beatae quo deleniti culpa. Soluta sunt quibusdam repellendus.</P>
+          </div>
+      </div>
+      </div>
+  </div>
+  
+  <div class="container padding-50">
+    <div class="row">
+      <div class="col-12 col-sm-6">
+        <figure>
+          <img src="img/sassy lady.jpg" class="img-fluid rounded" alt="">
+        </figure>
+      </div>
+      <div class="col-12 col-sm-6">
+        <h3>Abount Rochelle</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam qui eveniet a excepturi vero porro deserunt, rerum minus accusantium, quod quaerat architecto dolore consequuntur nesciunt possimus maxime mollitia labore consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, ipsum fugiat. Autem, repellendus minima quisquam vitae temporibus sunt soluta atque vel alias voluptates molestias placeat itaque praesentium impedit! Possimus, aspernatur! lore Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aspernatur porro quam ab? Enim incidunt vel qui velit, optio iusto eos quaerat blanditiis beatae officia quo commodi libero ratione iure.</p>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid">
+  <footer class="foot">
+          <div class="container foot">
+            <div class="row">
+              <figure class="col-12 col-sm-6 footer-img">
+                <img src="img/footer.png" class="img-fluid" alt="Rochelle logo">
+              </figure> 
+              <div class="col-12 col-sm-3">
+                <nav class="footer-nav">
+                  <ul>
+                    <li><a href="blog.com">Blog</a></li>
+                    <li><a href="books.html">Books</a></li>
+                    <li><a href="about.html">About Rochelle</a></li> 
+                    <li><a href="amazon.com">shop</a></li>
+                  </ul>
+                  <figure class="social">
+                    <a href=""><img src="img/Facebook logo.png" width="30" alt="Facebook icon"></a>
+                    <a href=""><img src="img/Twitter logo.png" width="30" alt="Twitter logo"></a>
+                    <a href=""><img src="img/Instagram logo.png" width="30" alt="Instagram logo"></a>
+                  </figure>
+                </nav>
+              </div>
+              <div class="col-12 col-sm-3">
+                <div class="address">
+                  <Strong>Write to Rochelle</Strong><br>
+                  Rochelle White <br>
+                  c/o Anonymous Publishing <br>
+                  123 Independence Avenue <br>
+                  Accra, Ghana 233
+                </div>
+              </div>        
+            </div>
+          </div>`
+        </footer>
+      </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
